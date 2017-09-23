86e92bfe94d255a557bc5c0f31feb127cd201260
yogay <yogay@xheomar>
2017-09-23 03:31:26 +0300

    Added properties file. Now bot can realize the commands /setNew1 /setNew2 and /setNew3

--- myTelegramBot/src/main/java/test/myTelegramBot/SimpleBot.java
+++ myTelegramBot/src/main/java/test/myTelegramBot/SimpleBot.java
@@ -2,7 +2,14 @@ package test.myTelegramBot;
 
 import org.telegram.telegrambots.exceptions.TelegramApiException;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.List;
+import java.util.Properties;
+
 import jersey.repackaged.com.google.common.collect.Lists;
 import test.myTelegramBot.json.Comment;
 import test.myTelegramBot.json.SportsMessages;
@@ -20,12 +27,19 @@ import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 public class SimpleBot extends TelegramLongPollingBot 
 {	
 	private static final String ADMIN_USERNAME = "Heorhi";
-	private static final String [] LIST_OF_PUBLICS = {"1413705", "1414425", "1414056"};
-	private long [] LIST_OF_LAST_MESSAGES = {0, 0, 0};
-	private int [] LIST_OF_STARTS = {0, 0, 0};
+	private static final String SET_NEW_1 = "/setNew1";
+	private static final String SET_NEW_2 = "/setNew2";
+	private static final String SET_NEW_3 = "/setNew3";
+	private volatile String [] LIST_OF_PUBLICS = {"1412670", "1412898", "1414425"};
+	private volatile long [] LIST_OF_LAST_MESSAGES = {0, 0, 0};
+	private volatile int [] LIST_OF_STARTS = {0, 0, 0};
 	private static final int ADMIN_USERID = 17872630;
 	private static final String PUBLIC_CHANNEL = "@systemfootball";
 	private volatile boolean isRunning = true;
+	private volatile boolean isDebug = false; 
+	
+	static File configFile = new File("config.properties");
+	static Properties props = new Properties();
 	
 	private SimpleBot(DefaultBotOptions options) 
 	{
@@ -51,7 +65,10 @@ public class SimpleBot extends TelegramLongPollingBot
 			TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
 			try 
 			{ 
-				telegramBotsApi.registerBot(new SimpleBot(options));
+				SimpleBot myBot = new SimpleBot(options);
+				System.out.println("Bot registration... ");
+				telegramBotsApi.registerBot(myBot);
+				myBot.getProperties();
 			} 
 			catch (TelegramApiException e) 
 			{
@@ -64,7 +81,10 @@ public class SimpleBot extends TelegramLongPollingBot
 			TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
 			try 
 			{ 
-				telegramBotsApi.registerBot(new SimpleBot());
+				SimpleBot myBot = new SimpleBot();
+				System.out.println("Bot registration... ");
+				telegramBotsApi.registerBot(myBot);
+				myBot.getProperties();
 			} 
 			catch (TelegramApiException e) 
 			{
@@ -74,6 +94,55 @@ public class SimpleBot extends TelegramLongPollingBot
 	}
  
 
+	private void getProperties() 
+	{
+		System.out.println("Reading configuration... ");
+		try 
+		{
+		    FileReader reader = new FileReader(configFile);
+		    
+		    props.load(reader);
+		 
+		    String id1 = props.getProperty("id1");
+		    LIST_OF_PUBLICS[0] = id1;
+		    String id2 = props.getProperty("id2");
+		    LIST_OF_PUBLICS[1] = id2;
+		    String id3 = props.getProperty("id3");
+		    LIST_OF_PUBLICS[2] = id3;
+		    
+		    System.out.println("The following IDs were read: " + id1 + " " + id2 + " " + id3);
+		    
+		    reader.close();
+		} 
+		catch (FileNotFoundException ex) 
+		{
+		    System.out.println("File Not Found");
+		} 
+		catch (IOException ex) 
+		{
+			System.out.println("I/O error");
+		}
+	}
+
+	private static void saveProperties() 
+	{
+		System.out.println("Saving configuration... ");
+		try 
+		{
+			FileWriter writer = new FileWriter(configFile);
+			props.store(writer, "bot settings");
+			writer.close();
+		} 
+		catch (FileNotFoundException ex) 
+		{
+		    System.out.println("File Not Found");
+		} 
+		catch (IOException ex) 
+		{
+			System.out.println("I/O error");
+		}
+	}
+	
 	public String getBotUsername() 
 	{
 		return "SystemFootballBot";
@@ -94,8 +163,37 @@ public class SimpleBot extends TelegramLongPollingBot
 			if (message != null && message.hasText()) 
 			{
 				System.out.println("Received the message: " + message.getText());
-				if (message.getText() != null && message.getText().equals("/start")) 
+				if (message.getText().startsWith(SET_NEW_1)) 
 				{
+					String newPublicId_1 = message.getText().split(" ")[1];
+					LIST_OF_PUBLICS[0] = newPublicId_1;
+					LIST_OF_STARTS[0] = 0;
+					sendMsg(message, "New 1st public is set to " + newPublicId_1);
+					props.setProperty("id1", newPublicId_1);
+					saveProperties();
+				}
+				if (message.getText().startsWith(SET_NEW_2)) 
+				{
+					String newPublicId_2 = message.getText().split(" ")[1];
+					LIST_OF_PUBLICS[1] = newPublicId_2;
+					LIST_OF_STARTS[1] = 0;
+					sendMsg(message, "New 2nd public is set to " + newPublicId_2);
+					props.setProperty("id2", newPublicId_2);
+					saveProperties();
+				}
+				if (message.getText().startsWith(SET_NEW_3)) 
+				{
+					String newPublicId_3 = message.getText().split(" ")[1];
+					LIST_OF_PUBLICS[2] = newPublicId_3;
+					LIST_OF_STARTS[2] = 0;
+					sendMsg(message, "New 3rd public is set to " + newPublicId_3);
+					props.setProperty("id3", newPublicId_3);
+					saveProperties();
+				}
+				if (message.getText().equals("/start") || message.getText().equals("/startDebug")) 
+				{
+					if (message.getText().equals("/start")) isDebug = false;
+					if (message.getText().equals("/startDebug")) isDebug = true;
 					isRunning = true;
 					new Thread() 
 					{
@@ -133,8 +231,14 @@ public class SimpleBot extends TelegramLongPollingBot
 									for (Comment comment: listComment)
 									{
 										LIST_OF_LAST_MESSAGES[publicId] = comment.getId();
-										sendMessageToChannel(message, app.getMessageFromComment(comment));
-										//sendMsg(message, app.getMessageFromComment(comment));
+										if (isDebug)
+										{
+											sendMsg(message, app.getMessageFromComment(comment));
+										}
+										else
+										{
+											sendMessageToChannel(message, app.getMessageFromComment(comment));
+										}
 									}
 									LIST_OF_STARTS[publicId] += 1;
 								}
@@ -158,8 +262,14 @@ public class SimpleBot extends TelegramLongPollingBot
 											else
 											{
 												LIST_OF_LAST_MESSAGES[publicId] = comment.getId();
-												sendMessageToChannel(message, app.getMessageFromComment(comment));
-												//sendMsg(message, app.getMessageFromComment(comment));
+												if (isDebug)
+												{
+													sendMsg(message, app.getMessageFromComment(comment));
+												}
+												else
+												{
+													sendMessageToChannel(message, app.getMessageFromComment(comment));
+												}
 											}
 											
 										}									
