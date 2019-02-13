5a6b1563d4c871d90b4799d5b65b9008bb125285
xheomar <yoga.yogich@gmail.com>
2019-02-13 04:12:05 +0300

    Ignore Tournament posts in VNLS

--- myTelegramBot/src/main/java/test/myTelegramBot/XmlReader.java
+++ myTelegramBot/src/main/java/test/myTelegramBot/XmlReader.java
@@ -7,6 +7,7 @@ import java.util.regex.Pattern;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 import test.myTelegramBot.xml.XmlItem;
 
@@ -26,32 +27,33 @@ public class XmlReader
 		String title, description, id="", link="", pubDate, channelName;
 		
 		doc = Jsoup.connect(BASE_URL + name + RSS_XML).get();
-		
-		// link & id
-		Element itemLink = doc.select("item > link").first();
+				
 		Pattern r = Pattern.compile("(\\d+)");
-		Matcher m = r.matcher(itemLink.html());
-	    if (m.find( )) 
-	    {				    
-	    	id = m.group(0);
+		
+	    Elements items = doc.select("item");
+	    
+	    // 1st item
+	    //System.out.println("1st title = " + items.get(0).select("title").first().html());
+	    // 2nd item
+	    //System.out.println("2nd title = " + items.get(1).select("title").first().html());
+	    int indexOfFirstItem = 0;
+	    title = items.get(indexOfFirstItem).select("title").first().html();
+	    
+	    if (title.contains("Турнир") || title.contains("турнир"))
+	    {
+	    	indexOfFirstItem = 1;
 	    }
-	    link = itemLink.html();
 	    
-	    // title 
-	    Element itemTitle = doc.select("item > title").first();
-	    title = itemTitle.html();
-	    
-	    // description 
-	    Element itemDescription = doc.select("item > description").first();
-	    description = itemDescription.html();
-	    
-	    // pubDate 
-	    Element itemPubDate = doc.select("item > pubDate").first();
-	    pubDate = itemPubDate.html();
-	    
-	    // channelName 
-	    Element rssChannelName = doc.select("channel > title").first();
-	    channelName = rssChannelName.html();
+	    link = items.get(indexOfFirstItem).select("link").first().html();
+		Matcher m = r.matcher(link);
+		if (m.find( )) 
+		{				    
+			id = m.group(0);
+		}
+	    title = items.get(indexOfFirstItem).select("title").first().html();
+	    description = items.get(indexOfFirstItem).select("description").first().html();
+	    pubDate = items.get(indexOfFirstItem).select("pubDate").first().html();
+	    channelName = doc.select("channel > title").first().html();
 	    
 	    XmlItem lastItem = new XmlItem(title, description, link, id, pubDate, channelName);
 	    
